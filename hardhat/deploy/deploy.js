2889dc152e40bfc88e2677fc8e658132fe35ed19
Nnam Darlington <Darlingtonnnam@gmail.com>
2022-07-11 00:54:34 +0100

    Update deploy.js

--- hardhat/deploy/deploy.js
+++ hardhat/deploy/deploy.js
@@ -8,3 +8,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     });
 };
 module.exports.tags = ['complete'];
+
+//0xb6BE61Dcd55E25fd6E3dB6aCB108eAb9490ffc37 - verifier
+//0x5b1d5A6AB7E25931b1cb90932215F3FC65d0d7D6 - minter
